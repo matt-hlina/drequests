f63fa43b29336bf7488fd35200f82f7b738e007e
Matthew Hlina <Matthew.Hlina@state.mn.us>
2025-11-07 17:18:00 -0600

    added a title to the .R file. reordered arguments to have names come before paths. removed code and replaced with respective functions, such as removing code for exporting data to Excel and replaced with create_excel() function after creating the function in a new .R file. renamed excel_list object to report_table to be more specific about what that object is.

--- R/datarequests.R
+++ R/datarequests.R
@@ -1,4 +1,6 @@
-#' Check data request
+############################################################################
+# Data request function for when sentencing info is asked to be grouped by CHS
+############################################################################
 
 #' @param data 'data.frame' of Sentencing Guidelines public data set.
 #' @param case_list_path pathway where the case list will be saved.
@@ -18,95 +20,28 @@
 #' @export
 
 chs_data_request <- function(data,
-                             case_list_path,
                              case_list_name,
-                             output_path,
+                             case_list_path,
                              output_name,
+                             output_path,
                              ...) {
 
   filters <- list(...)
-  df1 <- requested_cases(data, filters) # function found in utils.R, filter_function
 
   ###########################################################################
   # Everything below is pipeline
   ###########################################################################
 
   # Create case list #
-  data_set <- df1 %>%
-    dplyr::mutate(dplyr::across(
-      c(
-        county, Agecat, sex, race, severity, typecust, presumpt, plea,
-        inctype, stayexec, impose, condconf, consec, dispdep, durdep,
-        cnsdep, reason1, reason2, reason3, reason4,
-        preason1, preason2, preason3, Offense
-      ),
-      as.factor
-    )
-      severity = dplyr::case_when(
-        severity == "51" ~ "D1",
-        severity == "52" ~ "D2",
-        severity == "53" ~ "D3",
-        severity == "54" ~ "D4",
-        severity == "55" ~ "D5",
-        severity == "56" ~ "D6",
-        severity == "57" ~ "D7",
-        severity == "58" ~ "D8",
-        severity == "59" ~ "D9",
-        TRUE ~ severity),
-    ) %>%
-    dplyr::arrange(district, county, jlname, sentyear) %>%
-    dplyr::select(
-      "Conviction Statute" = convstat,
-      "Offense Name" = Offense_Title,
-      "Year Sentenced" = sentyear,
-      "District" = district,
-      "County" = county,
-      "Judge Last Name" = jlname,
-      "Judge First Name" = jfname,
-      "Case Number" = dcnum,
-      "Count Number" = count,
-      "Attempt" = attempt,
-      "Conspiracy" = conspir,
-      "Age Category" = Agecat,
-      "Sex" = sex,
-      "Race" = race,
-      "Offense Date" = doff,
-      "Severity Level" = severity,
-      "Type of Custody Offender on at Time of Offense" = typecust,
-      "Total Criminal History Points" = histall,
-      "Criminal History Score" = history,
-      "Presumptive Disposition" = presumpt,
-      "Presumptive Duration (months)" = time,
-      "Lower Range" = Mintime,
-      "Upper Range" = Maxtime,
-      "Plea" = plea,
-      "Pronounced Incarceration Type" = inctype,
-      "Pronounced Confinement (months)" = confine,
-      "Stay of Execution" = stayexec,
-      "Stay of Imposition" = impose,
-      "Length of Stay" = staylnth,
-      "Conditional Confinement (days)" = condconf,
-      "Received Consecutive Sentence" = consec,
-      "Total Consecutive Sentence (months)" = aggsentc,
-      "Dispositional Departure" = dispdep,
-      "Durational Departure" = durdep,
-      "Consecutive Departure" = cnsdep,
-      "Departure Reason 1" = reason1,
-      "Departure Reason 2" = reason2,
-      "Departure Reason 3" = reason3,
-      "Departure Reason 4" = reason4,
-      "Plea Reason 1" = preason1,
-      "Plea Reason 2" = preason2,
-      "Plea Reason 3" = preason3,
-      "Penalty Statute" = penaltystat
-    )
 
+  # filter for cases of interest to the requester
+  df1 <- requested_cases(data, filters) # function found in filter-function.R
 
-  excel_file <- file.path(case_list_path, case_list_name)
+  # select only the necessary variables for the final case list
+  data_set <- case_list(data, df1) # function found in final-case-list-function.R
 
-  writexl::write_xlsx(data_set, path = excel_file, col_names = TRUE)
-
-  file.show(path = excel_file)
+  # save the case list under the directed pathway with the directed name
+  create_excel(data_set, case_list_name, case_list_path)
 
   ########################################################################
   # Create report tables #
@@ -522,7 +457,7 @@ chs_data_request <- function(data,
   ###########################################################################
   # Return final result in Excel file
   ###########################################################################
-  excel_list <- list(
+  report_table <- list(
     "table" = final_chs_table,
     "mit-disp-dep-reas" = mdd_reasons,
     "mit-disp-dep-plea-reas" = mit_disp_plea_df,
@@ -530,15 +465,11 @@ chs_data_request <- function(data,
     "mit-dur-dep-plea-reas" = mit_dur_plea_df
   )
 
-  excel_file <- file.path(output_path, output_name)
 
-  writexl::write_xlsx(excel_list, path = excel_file, col_names = TRUE)
+  # save the report table output under the directed pathway with the directed name
+  create_excel(report_table, output_name, output_path)
 
-  file.show(path = excel_file)
 }
-
-
-
 
 
 
